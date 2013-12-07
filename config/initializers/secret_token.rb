907f11821fdea7311b0063529da66212f63eafd7
Peter Kennedy <peterjskennedy@gmail.com>
2013-12-07 12:52:32 -0500

    started project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whale::Application.config.secret_key_base = 'e3fae97aaea7829abbe4a5debfb1ea408a0bba26535672853bcefc453562fc29e6a7b694f73207ec92a7059523fd05b2917b2b4f0074ee303d93068c5f24488a'
